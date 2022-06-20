7272b0bf3ffb9b13cc33085ef3987dccfb8dd7b8
Dinis Costa <scorpaust@hotmail.com>
2022-06-21 00:10:36 +0100

    Setting up routing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import Directory from "./components/directory/directory.component";
+import Home from "./routes/home/home.component";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => { 
 
-  const categories = [
-    {
-      id: 1,
-      title: 'Desenvolvimento Web',
-      subtitle: 'Webdesign, Aplicações Web e Landing Pages',
-      imageUrl: 'https://images.pexels.com/photos/5483071/pexels-photo-5483071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-    },
-    {
-      id: 2,
-      title: 'Produção Musical',
-      subtitle: 'Mistura e Masterização de Temas',
-      imageUrl: 'https://images.pexels.com/photos/257904/pexels-photo-257904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-    },
-    {
-      id: 3,
-      title: 'Life Coach',
-      subtitle: 'Terapias Holísticas, Constelações Familiares e Consultoria Pessoal',
-      imageUrl: 'https://images.pexels.com/photos/4672438/pexels-photo-4672438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-    }
-  ]
-
   return(
-      <Directory categories={categories} />
+    <Routes>
+      <Route path="/"  element={<Home />} />
+    </Routes>
     )
 }
 
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,31 @@
+import Directory from "../../components/directory/directory.component";
+
+const Home = () => { 
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Desenvolvimento Web',
+      subtitle: 'Webdesign, Aplicações Web e Landing Pages',
+      imageUrl: 'https://images.pexels.com/photos/5483071/pexels-photo-5483071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    },
+    {
+      id: 2,
+      title: 'Produção Musical',
+      subtitle: 'Mistura e Masterização de Temas',
+      imageUrl: 'https://images.pexels.com/photos/257904/pexels-photo-257904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    },
+    {
+      id: 3,
+      title: 'Life Coach',
+      subtitle: 'Terapias Holísticas, Constelações Familiares e Consultoria Pessoal',
+      imageUrl: 'https://images.pexels.com/photos/4672438/pexels-photo-4672438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    }
+  ]
+
+  return(
+      <Directory categories={categories} />
+    )
+}
+
+export default Home;
