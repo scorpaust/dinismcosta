99598a3275389fe1efca4cd2be2a56cb8a0fb465
Dinis Costa <scorpaust@hotmail.com>
2022-06-21 23:53:19 +0100

    Navigation Bar and Logo

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,4 +1,5 @@
 import Directory from "../../components/directory/directory.component";
+import { Outlet } from "react-router-dom";
 
 const Home = () => { 
 
@@ -24,7 +25,10 @@ const Home = () => {
   ]
 
   return(
-      <Directory categories={categories} />
+      <div>
+        <Outlet />
+        <Directory categories={categories} />
+      </div>
     )
 }
 
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,29 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to="/">
+                    <div className="logo-text">Dinis Costa</div>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to="/sobre">
+                        SOBRE MIM
+                    </Link>
+                    <Link className="nav-link" to="/servicos">
+                        SERVIÇOS
+                    </Link>
+                    <Link className="nav-link" to="/servicos">
+                        CONTATO
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    );
+}
+
+export default Navigation;
