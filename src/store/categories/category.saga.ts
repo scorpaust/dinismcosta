446113c4ab8b0c1d73b4c66a7664a61b4782f017
scorpaust <scorpaust@hotmail.com>
2025-09-27 21:30:10 +0100

    Static images fix

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,21 +1,54 @@
-import { takeLatest, all, call, put } from 'typed-redux-saga/macro';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { fetchCategoriesSuccess, fetchCategoriesFailed } from './category.action';
-import { CATEGORIES_ACTION_TYPES } from './category.types';
+import { takeLatest, all, call, put } from "typed-redux-saga/macro";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import {
+  fetchCategoriesSuccess,
+  fetchCategoriesFailed,
+} from "./category.action";
+import { CATEGORIES_ACTION_TYPES, Category } from "./category.types";
+import portfolioSiteImage from "../../assets/portfolio_site.png";
+import mesaRadionicaImage from "../../assets/mesa_radionica_apometria.jpg";
+
+const SERVICE_IMAGE_OVERRIDES: Record<string, string> = {
+  "Sítio para Apresentação de Portfolio": portfolioSiteImage,
+  "Mesa Radiónica de Apometria": mesaRadionicaImage,
+};
+
+const applyServiceImageOverrides = (categories: Category[]): Category[] =>
+  categories.map((category) => ({
+    ...category,
+    items: category.items.map((item) => {
+      const overrideUrl = SERVICE_IMAGE_OVERRIDES[item.name];
+      if (!overrideUrl) {
+        return item;
+      }
+
+      return {
+        ...item,
+        imageUrl: overrideUrl,
+      };
+    }),
+  }));
 
 export function* fetchCategoriesAsync() {
-    try {
-        const categoriesArray = yield* call(getCategoriesAndDocuments, 'categories');
-        yield* put(fetchCategoriesSuccess(categoriesArray));
-      } catch (error) {
-        yield* put(fetchCategoriesFailed(error as Error));
-      }
+  try {
+    const categoriesArray = yield* call(
+      getCategoriesAndDocuments,
+      "categories"
+    );
+    const normalizedCategories = applyServiceImageOverrides(categoriesArray);
+    yield* put(fetchCategoriesSuccess(normalizedCategories));
+  } catch (error) {
+    yield* put(fetchCategoriesFailed(error as Error));
+  }
 }
 
 export function* onFetchCategories() {
-    yield* takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
+  yield* takeLatest(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START,
+    fetchCategoriesAsync
+  );
 }
 
 export function* categoriesSaga() {
-    yield* all([call(onFetchCategories)])
+  yield* all([call(onFetchCategories)]);
 }
